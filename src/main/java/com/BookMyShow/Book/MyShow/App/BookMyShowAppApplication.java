95fa20dc9263623393dbd74800b0d213cc6bfc21
DeepakGitHub050 <ds1008076@gmail.com>
2024-06-24 21:46:21 +0530

    First commit

---
+++ src/main/java/com/BookMyShow/Book/MyShow/App/BookMyShowAppApplication.java
@@ -0,0 +1,13 @@
+package com.BookMyShow.Book.MyShow.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyShowAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BookMyShow/Book/MyShow/App/BookMyShowAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BookMyShow.Book.MyShow.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
